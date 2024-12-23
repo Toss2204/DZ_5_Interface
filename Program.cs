aa45a02b9acc08bbde3c93154c6d3a87a99b0301
Toss2204 <izotov7428@gmail.com>
2024-12-23 18:11:48 +0300

    Создал один обх-ъект и его ссылку передал в переменные разных типов интерфейсов

--- Program.cs
+++ Program.cs
@@ -5,9 +5,11 @@ namespace DZ_5_Interface
     {
         static void Main(string[] args)
         {
+            var quad = new Quadcopter();
+            Console.WriteLine("Сам объект класса ничего не говорит, но через призму интерфейсов можно пообщаться");
 
             Console.WriteLine("IRobot говорит:");
-            IRobot quad1 = new Quadcopter();
+            IRobot quad1 = quad;//new Quadcopter();
             Console.WriteLine(quad1.GetInfo());
             Console.WriteLine(quad1.GetRobotType());
             Console.WriteLine("Компоненты робота:");
@@ -18,13 +20,13 @@ namespace DZ_5_Interface
             Console.WriteLine();
 
             Console.WriteLine("IChargeable говорит:");
-            IChargeable quad2 = new Quadcopter();
+            IChargeable quad2 = quad;//new Quadcopter();
             quad2.Charge();
             Console.WriteLine(quad2.GetInfo());
             Console.WriteLine();
 
             Console.WriteLine("IFlyingRobot говорит:");
-            IFlyingRobot quad3 = new Quadcopter();
+            IFlyingRobot quad3 = quad;//new Quadcopter();
             Console.WriteLine(quad3.GetRobotType());
             Console.WriteLine();
 
